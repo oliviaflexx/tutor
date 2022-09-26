0ec1b7485d2fb3ed96ecd2210eea113af17fc175
Olivia <oliviaflexx@gmail.com>
2022-09-26 17:11:22 -0400

    fix router

--- src/components/ReactRouter.js
+++ src/components/ReactRouter.js
@@ -1,22 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const ReactRouterSetup = () => {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <div>Home</div>
-        </Route>
-        <Route path="/about">
-          <div>About</div>
-        </Route>
-        <Route path="*">
-          <div> doesn't exist</div>
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<div>Home</div>}></Route>
+        <Route path="/about" element={<div>About</div>}></Route>
+        <Route path="*" element={<div> doesn't exist</div>}></Route>
+      </Routes>
     </Router>
   );
 };
