1cb2aeea3c25ff70c6c86071ab33b78fe5d7a46c
Olivia <oliviaflexx@gmail.com>
2022-09-26 17:02:55 -0400

    fix navbar import and try helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import ReactRouterSetup from "./components/ReactRouter";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
-      <ReactRouterSetup/>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Admissions Coach and English Tutor</title>
+        <meta name="description" content="Tutoring" />
+        <meta name="theme-color" content="#00A5CF" />
+      </Helmet>
+      <ReactRouterSetup />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
     <div>
       <NavLink to="/">Home</NavLink>
@@ -9,3 +9,5 @@ export const Navbar = () => {
     </div>
   );
 };
+
+export default Navbar;
